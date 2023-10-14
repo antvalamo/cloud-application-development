2669f0ec6c48b3698ef0a62e425491e4fd21de3b
antvalamo <antvalamo@alum.us.es>
2023-10-14 11:41:31 +0200

    a

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,21 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import './App.css';
 
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const cors = require('cors');
+
+const config = require('./config');
+
+var app = express();
+
+app.use(cors(
+  config.application.cors.server
+));;
+
 function App() {
   const [file, setFile] = useState(null);
   const [name, setName] = useState('');
